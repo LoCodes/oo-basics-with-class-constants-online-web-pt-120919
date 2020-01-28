6677c132b271e5cd11674a8a0ed9474aa064ff9a
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-28 00:07:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS.size = ["Uggs", "Rainbow", "Nike"]
 
   def initialize(brand)
     @brand = brand
